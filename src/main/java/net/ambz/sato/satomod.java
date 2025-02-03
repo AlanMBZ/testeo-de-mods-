13888823417eb8398f5001209cde972167588422
AlanMBZ <alan200470@hotmail.com>
2025-02-02 20:50:20 -0600

    Integracion item

--- src/main/java/net/ambz/sato/satomod.java
+++ src/main/java/net/ambz/sato/satomod.java
@@ -1,6 +1,9 @@
-package net.ambz.test;
+package net.ambz.sato;
 
 import com.mojang.logging.LogUtils;
+import net.ambz.sato.item.ModCreativeModTabs;
+import net.ambz.sato.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -15,17 +18,21 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(testmod.MOD_ID)
-public class testmod
+@Mod(satomod.MOD_ID)
+public class satomod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "testmod";
+    public static final String MOD_ID = "satomod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public testmod(FMLJavaModLoadingContext context){
+    public satomod(FMLJavaModLoadingContext context){
         IEventBus modEventBus = context.getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -40,7 +47,9 @@ public class testmod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event){
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
