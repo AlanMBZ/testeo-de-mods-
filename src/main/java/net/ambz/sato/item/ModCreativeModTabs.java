783066696be462a5bd8a2a18cf7bab6e1281bbb2
AlanMBZ <alan200470@hotmail.com>
2025-02-04 14:45:09 -0600

    Creacion de recetas basicas

--- src/main/java/net/ambz/sato/item/ModCreativeModTabs.java
+++ src/main/java/net/ambz/sato/item/ModCreativeModTabs.java
@@ -23,6 +23,7 @@ public class ModCreativeModTabs {
                     .title(Component.translatable("creativetab.tutorial_tab"))
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                     })
